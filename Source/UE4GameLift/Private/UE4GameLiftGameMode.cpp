d62fde49de7f3509b77e211bb58a9a31500373cc
ZX_Diablo <yakut2004@bk.ru>
2020-02-10 11:53:50 +0500

    Move GameMode setup to Blueprint

--- Source/UE4GameLift/Private/UE4GameLiftGameMode.cpp
+++ Source/UE4GameLift/Private/UE4GameLiftGameMode.cpp
@@ -2,14 +2,6 @@
 
 #include "UE4GameLiftGameMode.h"
 
-#include "UE4GameLiftCharacter.h"
-#include "UObject/ConstructorHelpers.h"
-
 AUE4GameLiftGameMode::AUE4GameLiftGameMode()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
