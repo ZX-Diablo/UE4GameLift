775b375396bb5d26fb599a967f46a53a755cfd7f
ZX_Diablo <yakut2004@bk.ru>
2020-02-10 12:35:58 +0500

    Move Character settings to Blueprint

--- Source/UE4GameLift/Private/UE4GameLiftCharacter.cpp
+++ Source/UE4GameLift/Private/UE4GameLiftCharacter.cpp
@@ -3,33 +3,17 @@
 #include "UE4GameLiftCharacter.h"
 
 #include "Camera/CameraComponent.h"
-#include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
 
 AUE4GameLiftCharacter::AUE4GameLiftCharacter()
 {
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
-
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
-	GetCharacterMovement()->JumpZVelocity = 600.f;
-	GetCharacterMovement()->AirControl = 0.2f;
-
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f;
-	CameraBoom->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	FollowCamera->bUsePawnControlRotation = false;
 }
 
 void AUE4GameLiftCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
